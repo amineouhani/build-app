6728f252c68b70fa51a2174996bade214985c787
aminenoidea <amineprojet7@gmail.com>
2020-10-17 14:27:05 +0200

    fucked up agains

--- src/main.js
+++ src/main.js
@@ -1,168 +1,62 @@
-/**
- * Import modules
- */
 import { BrowserWindow, Menu, app } from 'electron';
 import path from 'path';
-//import url from 'url';
+const config = require('./settings.json');
 
-
-/**
- * Enable Widevine Content Decryption Module
- */
 app.commandLine.appendSwitch('widevine-cdm-path', path.join(__dirname, 'lib/widewine-cdm/1.4.8.903/widevinecdmadapter.plugin'));
 app.commandLine.appendSwitch('widevine-cdm-version', '1.4.8.903');
 
-
-/**
- * Define global variable for the main window
- */
 let mainWindow;
 
-
-/**
- * Create app menu template
- */
 const menuTemplate = [{
-    label: 'Message',
-    submenu: [{
-        label: 'About Application',
-        role: 'about',
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Hide Message',
-        accelerator: 'Command+H',
-        role: 'hide',
-    }, {
-        label: 'Quit Message',
-        accelerator: 'Command+Q',
-        role: 'quit',
-    }],
-}, {
-    label: 'Edit',
-    submenu: [{
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        role: 'undo',
-    }, {
-        label: 'Redo',
-        accelerator: 'Command+Y',
-        role: 'redo',
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        role: 'cut',
-    }, {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        role: 'copy',
-    }, {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        role: 'paste',
-    }, {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        role: 'selectall',
-    }],
-}, {
-    label: 'View',
     submenu: [{
         label: 'Reload',
         accelerator: 'Command+R',
-        click: (menuItem, mainWindow) => {
+        click: (mainWindow) => {
             if (mainWindow) {
                 mainWindow.reload();
             }
         },
     }],
-}, {
-    label: 'Window',
-    submenu: [{
-        label: 'Zoom',
-        role: 'zoom',
-    }, {
-        label: 'Minimize',
-        accelerator: 'Command+M',
-        role: 'minimize',
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Toggle Frameless Window',
-        accelerator: 'Command+E',
-        type: 'checkbox',
-        checked: false,
-        click: (menuItem, mainWindow) => {
-            createFramelessWindow();
-        },
-    }, {
-        label: 'Float on Top',
-        accelerator: 'Command+T',
-        type: 'checkbox',
-        checked: false,
-        click: (menuItem, mainWindow) => {
-            const isChecked = !menuItem.checked;
-
-            mainWindow.setAlwaysOnTop(!isChecked);
-        },
-    }, {
-        type: 'separator',
-    }, {
-        label: 'Show Inspector',
-        accelerator: 'Command+Alt+I',
-        click: (menuItem, mainWindow) => {
-            mainWindow.webContents.openDevTools();
-        },
-    }],
 }];
 
-
-/**
- * Define window settings
- */
 const windowSettings = {
-    width: 1360,
-    height: 768,
-    backgroundColor: '#FFF',
-    useContentSize: true,
-    resizable: true,
-    center: true,
-    alwaysOnTop: false,
-    frame: true,
-    title: 'Messages',
+    width: config.width,
+    height: config.height,
+    backgroundColor: config.backgroundColor,
+    useContentSize: config.contentSize,
+    resizable: config.resizable,
+    center: config.isCenter,
+    alwaysOnTop: config.alwaysTop,
+    frame: config.frame,
+    title: config.title,
     webPreferences: {
-        nodeIntegration: false,
-        plugins: true,
+        nodeIntegration: config.nodeIntegration,
+        plugins: config.usePlugin,
+        thickFrame: config.thickFrame
     },
 };
-
-/**
- * Activate app
- */
 const activateApp = () => {
-    createFramelessWindow();
+    createWindow();
     Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
 };
-
-
-/**
- * Create new frameless window
- */
-const createFramelessWindow = () => {
+const createWindow = () => {
     if (mainWindow) {
         mainWindow.close();
     }
 
-    windowSettings.frame = true;
+    windowSettings.frame = config.allowFrame;
     mainWindow = new BrowserWindow(windowSettings);
-    mainWindow.loadURL('https://messages.google.com/');
+    switch (config.multipleBuild) {
+        case "Message":
+            mainWindow.loadURL('https://messages.google.com/');
+            break;
+        case "Instagram":
+            mainWindow.loadURL('https://www.instagram.com/');
+            break;
+        case "Linkedin":
+            mainWindow.loadURL('https://linkedin.com/');
+            break;
+    }
     mainWindow.webContents.on('did-finish-load', () => {});
 };
-
-
-/**
- * Activate app when ready
- */
 app.on('ready', activateApp);
